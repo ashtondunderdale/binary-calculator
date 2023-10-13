73d330bd2a4d1378b14c2bb76a813fbe5c5fce75
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-13 19:25:50 +0100

    dark mode
    full configured dark mode

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("calculateButton").addEventListener("click", calculateAndDisplayResult);
     document.getElementById("resetButton").addEventListener("click", resetFields);
-    
+    document.getElementById("darkModeButton").addEventListener("click", toggleDarkMode);
+
+    // Initialize dark mode status
+    var isDarkMode = false;
+
+    // Check for dark mode on page load and update button text
+    updateDarkModeButtonText();
+
     function calculateAndDisplayResult() {
         var num1 = document.getElementById("binOne").value;
         var num2 = document.getElementById("binTwo").value;
-        var operation = document.getElementById("baseSelect").value; 
-        
+        var operation = document.getElementById("baseSelect").value;
+
         var result;
 
         if (operation === "Addition") {
@@ -26,14 +33,14 @@ document.addEventListener("DOMContentLoaded", function () {
         var sum = int1 + int2;
         return sum.toString(2);
     }
-    
+
     function subtractBinaryNumbers(binary1, binary2) {
         var int1 = parseInt(binary1, 2);
         var int2 = parseInt(binary2, 2);
         var difference = int1 - int2;
         return difference.toString(2);
     }
-    
+
     function multiplyBinaryNumbers(binary1, binary2) {
         var int1 = parseInt(binary1, 2);
         var int2 = parseInt(binary2, 2);
@@ -45,5 +52,16 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("binOne").value = "";
         document.getElementById("binTwo").value = "";
         document.getElementById("binResult").value = "";
+    }
+
+    function toggleDarkMode() {
+        document.body.classList.toggle("dark-mode");
+        isDarkMode = !isDarkMode; 
+        updateDarkModeButtonText(); 
+    }
+
+    function updateDarkModeButtonText() {
+        var darkModeButton = document.getElementById("darkModeButton");
+        darkModeButton.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
     }
 });
