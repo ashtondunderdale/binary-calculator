19bffa452bd2aad409b139789708959ea2efc108
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-10-14 12:12:40 +0100

    fixed dark mode save across pages

--- indexScripts.js
+++ indexScripts.js
@@ -2,6 +2,24 @@ document.addEventListener("DOMContentLoaded", function () {
     var checkbox = document.getElementById("myCheckbox");
     var biLabel = document.querySelector(".biLbl");
     var deLabel = document.querySelector(".DeLbl");
+    var darkModeButton = document.getElementById("darkModeButton");
+
+    function updateDarkModePreference() {
+        if (isDarkMode) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("dark-mode-preference", "dark");
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("dark-mode-preference", "light");
+        }
+    }
+
+    // Check for the dark mode preference in local storage and apply it
+    var savedDarkModePreference = localStorage.getItem("dark-mode-preference");
+    if (savedDarkModePreference === "dark") {
+        isDarkMode = true;
+        updateDarkModePreference();
+    }
 
     checkbox.addEventListener("change", function () {
         biLabel.style.display = checkbox.checked ? "inline" : "none";
@@ -9,16 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     var isDarkMode = false;
-    var darkModeButton = document.getElementById("darkModeButton");
 
     function updateDarkModeButtonText() {
         darkModeButton.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
     }
 
     function toggleDarkMode() {
-        document.body.classList.toggle("dark-mode");
         isDarkMode = !isDarkMode;
         updateDarkModeButtonText();
+        updateDarkModePreference(); 
     }
 
     darkModeButton.addEventListener("click", toggleDarkMode);
